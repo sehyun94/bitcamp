4dd27bfc3f04742c409f7cd08841bb6ca63c900b
sehyun94 <k52140@naver.com>
2017-10-12 09:20:29 +0900

    aa

--- java-basic/src/main/java/bitcamp/java100/Test15_11_1.java
+++ java-basic/src/main/java/bitcamp/java100/Test15_11_1.java
@@ -1,9 +1,17 @@
+// ## 연산자 - 비트 연산자 (&, |, ~, ^) 응용
+// - 비트 연산자의 활용 예
+//
+
 package bitcamp.java100;
 
-public class Test15_11_1{
-    public static void main(String[] args)  {
-        
-//디렉토리여부, 소유주/그룹/기타 사용자의 사용권한(읽기,쓰기 실행 권한 정보)
+public class Test15_11_1 {
+
+    public static void main(String[] args) {
+// 임무: 파일의 정보를 저장하라!
+// - 디렉토리여부, 소유주/그룹/기타 사용자의 읽기, 쓰기, 실행 권한 정보
+//
+// 방법1: 클래스를 이용하여 처리        
+        // => 메모리 설계
         class Privilege {
             boolean isDirectory;
             boolean ownerRead;
@@ -17,8 +25,10 @@ public class Test15_11_1{
             boolean otherExe;
         }
         
+        // => Privilege 설계도에 따라 메모리를 준비하고 그 메모리의 주소를 저장한다.
         Privilege p1 = new Privilege();
         
+        // => 메모리 주소(레퍼런스)를 가지고 해당 메모리의 각 항목에 값을 넣는다.
         p1.isDirectory = false;
         p1.ownerRead = true;
         p1.ownerWrite = true;
@@ -30,6 +40,7 @@ public class Test15_11_1{
         p1.otherWrite = false;
         p1.otherExe = true;
         
+        // => 사용 권한 검사
         System.out.print(p1.isDirectory ? "d" : "-");
         System.out.print(p1.ownerRead ? "r" : "-");
         System.out.print(p1.ownerWrite ? "w" : "-");
@@ -40,10 +51,26 @@ public class Test15_11_1{
         System.out.print(p1.otherRead ? "r" : "-");
         System.out.print(p1.otherWrite ? "w" : "-");
         System.out.print(p1.otherExe ? "x" : "-");
-      
+        System.out.println();
         
     }
-}   
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
--- java-basic/src/main/java/bitcamp/java100/Test15_7.java
+++ java-basic/src/main/java/bitcamp/java100/Test15_7.java
@@ -1,16 +1,17 @@
+// ## 연산자 - 관계 연산자(==, !=, <, <=, >, >=)
+// - 관계 연산자의 사용법을 알아보자!
+//
 
 package bitcamp.java100;
 
-public class Test15_7{
+public class Test15_7 {
 
-    public static void main(String[] args)  {
-
+    public static void main(String[] args) {
         System.out.printf("10 == 20 ? %b\n", 10 == 20);
-        System.out.printf("10 != 20 ? %b\n", 10 != 20); //(!= 같지않다)
+        System.out.printf("10 != 20 ? %b\n", 10 != 20);
         System.out.printf("10 < 20 ? %b\n", 10 < 20);
         System.out.printf("10 <= 20 ? %b\n", 10 <= 20);
         System.out.printf("10 > 20 ? %b\n", 10 > 20);
         System.out.printf("10 >= 20 ? %b\n", 10 >= 20);
     }
-}    
-
+}
